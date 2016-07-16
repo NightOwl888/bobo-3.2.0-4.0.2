d4449d51105ab27da7f0b60d619dcad122e0bd1f
Shad Storhaug <shad@shadstorhaug.com>
2016-07-17 04:39:00 +0700

    main r-z exclude facets 4.0.2

--- bobo-browse/src/main/java/com/browseengine/bobo/service/DefaultBrowseQueryParser.java
+++ bobo-browse/src/main/java/com/browseengine/bobo/service/DefaultBrowseQueryParser.java
@@ -5,90 +5,71 @@ import java.util.Arrays;
 
 import org.apache.lucene.search.DocIdSet;
 
-import com.kamikaze.docidset.impl.AndDocIdSet;
-import com.kamikaze.docidset.impl.NotDocIdSet;
-import com.kamikaze.docidset.impl.OrDocIdSet;
+import com.browseengine.bobo.docidset.AndDocIdSet;
+import com.browseengine.bobo.docidset.NotDocIdSet;
+import com.browseengine.bobo.docidset.OrDocIdSet;
 
 public class DefaultBrowseQueryParser implements BrowseQueryParser {
 
-	public DocIdSet parse(SelectionNode[] selectionNodes,SelectionNode[] notSelectionNodes,int maxDoc) {
-		DocIdSet docSet=null;
-		DocIdSet selSet=null;
-		
-		if (selectionNodes!=null && selectionNodes.length>0)
-		{
-			ArrayList<DocIdSet> selSetList=new ArrayList<DocIdSet>(selectionNodes.length);
-			for (SelectionNode selectionNode : selectionNodes)
-			{				
-				DocIdSet ds=selectionNode.getDocSet();
-				
-				if (ds!=null)
-				{
-					selSetList.add(ds);
-				}
-			}
-			
-			if (selSetList.size()>0)
-			{
-				if (selSetList.size()==1)
-				{
-					selSet=selSetList.get(0);
-				}
-				else
-				{
-					selSet=new AndDocIdSet(selSetList);
-				}
-			}
-		}
-			
-		DocIdSet notSelSet=null;
-		
-		if (notSelectionNodes!=null && notSelectionNodes.length > 0)
-		{
-			ArrayList<DocIdSet> notSelSetList=new ArrayList<DocIdSet>(notSelectionNodes.length);
-			for (SelectionNode selectionNode : notSelectionNodes)
-			{
-				DocIdSet ds=selectionNode.getDocSet();
-				
-				if (ds!=null)
-				{
-					notSelSetList.add(ds);
-				}
-				
-				if (notSelSetList.size()>0)
-				{
-					if (notSelSetList.size()==1)
-					{
-						notSelSet=notSelSetList.get(0);
-					}
-					else
-					{
-						notSelSet=new OrDocIdSet(notSelSetList);
-					}
-				}	
-			}
-		}
-		
-		if (notSelSet!=null)
-		{
-			notSelSet=new NotDocIdSet(notSelSet,maxDoc);
-		}
-		
-		if (selSet!=null && notSelSet!=null)
-		{
-			DocIdSet[] sets=new DocIdSet[]{selSet,notSelSet};
-			docSet=new AndDocIdSet(Arrays.asList(sets));
-		}
-		else if (selSet!=null)
-		{
-			docSet=selSet;
-		}
-		else if (notSelSet!=null)
-		{
-			docSet=notSelSet;
-		}
-		
-		
-		return docSet;
-	}
+  @Override
+  public DocIdSet parse(SelectionNode[] selectionNodes, SelectionNode[] notSelectionNodes,
+      int maxDoc) {
+    DocIdSet docSet = null;
+    DocIdSet selSet = null;
+
+    if (selectionNodes != null && selectionNodes.length > 0) {
+      ArrayList<DocIdSet> selSetList = new ArrayList<DocIdSet>(selectionNodes.length);
+      for (SelectionNode selectionNode : selectionNodes) {
+        DocIdSet ds = selectionNode.getDocSet();
+
+        if (ds != null) {
+          selSetList.add(ds);
+        }
+      }
+
+      if (selSetList.size() > 0) {
+        if (selSetList.size() == 1) {
+          selSet = selSetList.get(0);
+        } else {
+          selSet = new com.browseengine.bobo.docidset.AndDocIdSet(selSetList);
+        }
+      }
+    }
+
+    DocIdSet notSelSet = null;
+
+    if (notSelectionNodes != null && notSelectionNodes.length > 0) {
+      ArrayList<DocIdSet> notSelSetList = new ArrayList<DocIdSet>(notSelectionNodes.length);
+      for (SelectionNode selectionNode : notSelectionNodes) {
+        DocIdSet ds = selectionNode.getDocSet();
+
+        if (ds != null) {
+          notSelSetList.add(ds);
+        }
+
+        if (notSelSetList.size() > 0) {
+          if (notSelSetList.size() == 1) {
+            notSelSet = notSelSetList.get(0);
+          } else {
+            notSelSet = new OrDocIdSet(notSelSetList);
+          }
+        }
+      }
+    }
+
+    if (notSelSet != null) {
+      notSelSet = new NotDocIdSet(notSelSet, maxDoc);
+    }
+
+    if (selSet != null && notSelSet != null) {
+      DocIdSet[] sets = new DocIdSet[] { selSet, notSelSet };
+      docSet = new AndDocIdSet(Arrays.asList(sets));
+    } else if (selSet != null) {
+      docSet = selSet;
+    } else if (notSelSet != null) {
+      docSet = notSelSet;
+    }
+
+    return docSet;
+  }
 }
